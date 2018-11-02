cdb566ac63fa07daa3dc2673c85c9e10fa36b970
Blinky0815 <1518628+Blinky0815@users.noreply.github.com>
2018-11-02 13:01:58 +0100

    Update KinectBackgroundModel.h

--- new c++ version/KinectBackgroundModel.h
+++ new c++ version/KinectBackgroundModel.h
@@ -1,8 +1,20 @@
-/* 
- * File:   KinectBackgroundModel.h
- * Author: blinky0815
- *
- * Created on August 6, 2014, 6:19 PM
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * Copyright (C) 2014 Olaf Christ 
+ * email: christ_o@gmx.de
+ * 
+ * Tracking-by-growing-and-shrinking is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tracking-by-growing-and-shrinking is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef KINECTBACKGROUNDMODEL_H
